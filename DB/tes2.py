4942631fdc131ce1a7f62da17164efd2947389a9
galigaribaldi <galigaribaldi@live.com>
2020-04-15 01:30:46 -0500

    Version 0.5

--- DB/tes2.py
+++
@@ -1,7 +0,0 @@
-import sqlite3 as sql
-def insertarcalificacion_Bimestre1(campo1_B1,campo2_B1,campo3_B1, campo4_B1,promedio, estudiante_ID, materia_ID, profesor_ID):
-    con = sql.connect("baseConeyotl.db")
-    cursor = con.cursor()
-    cursor.execute("UPDATE inscripcion_grado SET campo1_B1=? ,campo2_B1=?, campo3_B1=?, campo4_B1=?, calificacion_B1=? WHERE estudiante_ID =? AND materia_ID=? AND profesor_ID=?", (campo1_B1,campo2_B1,campo3_B1, campo4_B1,promedio, estudiante_ID, materia_ID, profesor_ID))
-    con.commit()
-    con.close()
--- DB/tes3.py
+++
@@ -1,2 +0,0 @@
-import tes2 as coneccion
-coneccion.insertarcalificacion_Bimestre1(10,10,1,1,6.75,1,1,1)
