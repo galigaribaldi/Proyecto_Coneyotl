80727e084bc4bc557de360db8fd0ba95f76110ca
galigaribaldi <galigaribaldi@live.com>
2020-05-05 02:11:33 -0500

    Correos funcionando

--- worker.py
+++ worker.py
@@ -1,15 +1,18 @@
 import os
-
-import os
-import redis
-from rq import Worker, Queue, Connection
+import urlparse
+from redis import Redis
+from rq import Queue, Connection
+from rq.worker import HerokuWorker as Worker
 
 listen = ['high', 'default', 'low']
 
-#redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 redis_url = os.getenv('REDISTOGO_URL')
-print(redis_url)
-conn = redis.from_url(redis_url)
+if not redis_url:
+    raise RuntimeError('No hay variable')
+
+urlparse.uses_netloc.append('redis')
+url = urlparse.urlparse(redis_url)
+conn = Redis(host=url.hostname, port=url.port, db=0, password=url.password)
 
 if __name__ == '__main__':
     with Connection(conn):
