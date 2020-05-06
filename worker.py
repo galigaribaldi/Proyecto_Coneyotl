b7cc1ff604730194c82e51b66a4906e36eedda55
galigaribaldi <galigaribaldi@live.com>
2020-05-06 16:00:37 -0500

    Cambios al correo

--- worker.py
+++ worker.py
@@ -6,6 +6,19 @@ from rq import Worker, Queue, Connection
 listen = ['high', 'default', 'low']
 
 redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+redis_host = os.environ.get('REDIS_HOST', 'localhost')    
+# Channel layer definitions
+# http://channels.readthedocs.org/en/latest/deploying.html#setting-up-a-channel-backend
+CHANNEL_LAYERS = {
+    "default": {
+        # This example app uses the Redis channel layer implementation asgi_redis
+        "BACKEND": "asgi_redis.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [(redis_host, 6379)],
+        },
+        "ROUTING": "multichat.routing.channel_routing",
+    },
+}
 print("Este es el URL:", redis_url)
 conn = redis.from_url(redis_url)
 
